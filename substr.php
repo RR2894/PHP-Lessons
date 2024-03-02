0b014908e6cda870452f40ee3d0e0404c63546bb
Randolph <randolph2894@gmail.com>
2024-03-02 19:16:36 +0200

    update

--- substr.php
+++
@@ -1,8 +0,0 @@
-<?php
-// substr($target, $start, $lenght);
-$foo = "This is a string";
-print substr($foo, 5, 2); // returns is
-print substr($foo, 5); // returns is a string
-print "\n";
-// print substr($foo, -5);
-print sbustr($foo, 5, );
--- task.php
+++
@@ -1,5 +0,0 @@
-<?php
-$age = 18;
-$name = "Robert";
-$isStudent = true;
-print var_dump ($age, $name, $isStudent);
